4a92612e6a1a5a5e0dfc2dfe6db86f5232a107ae
Fenris <anatolijv236@gmail.com>
2021-12-19 18:18:07 +0300

    mobile, js

--- js/scripts.js
+++ js/scripts.js
@@ -2,43 +2,24 @@ $(document).ready(() => {
     /**
      * SLICK
      */
-    $('.stock__list').slick({
+    $('.popular').slick({
         infinite: true,
         arrows: false,
         dots: true,
-        slidesToShow: 2,
-        autoplay: true,
-        responsive: [
-            {
-                breakpoint: 1000,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-        ]
-    });
-
-    $('.staff__list').slick({
-        infinite: true,
-        arrows: true,
-        dots: true,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 2,
-        nextArrow: '<button type="button" class="slick-next"><svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg"><circle r="35" transform="matrix(-1 0 0 1 35 35)" fill="#F7F2EC"/><path d="M38 40.7485L36.8566 42L30.4615 35L36.8566 28L38 29.2515L32.7483 35L38 40.7485V40.7485Z" fill="black" fill-opacity="0.54"/></svg></button>',
-        prevArrow: '<button type="button" class="slick-prev"><svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg"><circle r="35" transform="matrix(-1 0 0 1 35 35)" fill="#F7F2EC"/><path d="M38 40.7485L36.8566 42L30.4615 35L36.8566 28L38 29.2515L32.7483 35L38 40.7485V40.7485Z" fill="black" fill-opacity="0.54"/></svg></button>',
         autoplay: true,
         responsive: [
             {
-                breakpoint: 1000,
+                breakpoint: 1200,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
+                    slidesToShow: 3,
                 }
             },
             {
                 breakpoint: 640,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                 }
             },
@@ -59,5 +40,33 @@ $(document).ready(() => {
     $('.menu-toggle-inner').on('click', () => {
         $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
         $('.headerNav_adaptive').toggleClass('header_opened');
+    });
+
+    /**
+     * FILE INPUT
+     */
+    $('input[type=file] ~ label').on('click', (e) => {
+        $(e.target).siblings('input').trigger('click');
+    });
+
+    $('input[type="file"]').on('change', (e) => {
+        let input = $(e.target);
+        let container = input.parents('form');
+        let name = e.currentTarget.files[0].name;
+
+        if (!name) {
+            return;
+        }
+
+        container.find(('.form__file-name')).text(name);
+        container.find('.btn__delete-file').css('display', 'inline-block');
+    });
+
+    $('.btn__delete-file').on('click', (e) => {
+        let parent = $(e.target).parents('form');
+
+        parent.find('input[type="file"]').val(null);
+        parent.find('.form__file-name').text('');
+        parent.find('.btn__delete-file').css('display', 'none');
     });
 });
